38e5c7df9dadff2a9446a2dcab1e7f627fe1ec6b
Evendashiell <slowsteady0412@gmail.com>
2023-10-17 00:40:00 +0530

    DOM3

---
+++ dom.js
@@ -0,0 +1,8 @@
+// QuerySelector
+
+var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor = 'green';
+var thirditem = document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.color = 'white';
+
+
