dd7717ecefee33773788b39f6089211898d5c539
Subrat <66966741+Evendashiell@users.noreply.github.com>
2023-10-17 00:43:31 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -5,4 +5,15 @@ seconditem.style.backgroundColor = 'green';
 var thirditem = document.querySelector('.list-group-item:nth-child(3)');
 thirditem.style.color = 'white';
 
+// QuerySelectorALL
+
+var list = document.querySelectorAll('li');
+list[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0; i<odd.length; i++)
+{
+    odd[i].style.backgroundColor = 'green';
+}
+
 
